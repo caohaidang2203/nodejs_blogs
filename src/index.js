c1be395192218fb3faa28d14f148be9f26f12fcb
caohaidang2203 <caohaidang2203@gmail.com>
2023-02-08 21:39:31 +0700

    three change

--- src/index.js
+++ src/index.js
@@ -5,10 +5,20 @@ const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes/index.js');
+//Xử lys dữ liệu từ form submit lên
+app.use(express.urlencoded({
+    extended: true
+}));
+//Xử lý dữ liệu qua việc sử dụng các thư viện trong js
+//XMLHttpRequest, fetch, axios,...
+app.use(express.json());
+
 //Xử lý khi các file tĩnh nó sẽ vào public path
 app.use(express.static(path.join(__dirname, 'public')));
+
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 //Template engine
 app.engine('hbs', engine({
@@ -17,14 +27,8 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource/views'));
 
-//route
-app.get('/', (req, res) => {
-    res.render('home');
-})
-
-app.get('/news', (req, res) => {
-    res.render('news');
-})
+// Routes init
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
